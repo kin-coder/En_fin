e8683915500d3fe4545a45af7a8e0585a2517746
RAMAHAFALY EDMOND <ramahafaly2.0@gmail.com>
2019-12-16 13:43:18 +0300

    edit back

--- db/migrate/20191213142455_create_prestataires.rb
+++
@@ -1,8 +0,0 @@
-class CreatePrestataires < ActiveRecord::Migration[6.0]
-  def change
-    create_table :prestataires do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_13_145419) do
+ActiveRecord::Schema.define(version: 2019_12_16_103528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,11 @@ ActiveRecord::Schema.define(version: 2019_12_13_145419) do
   end
 
   create_table "prestataires", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name"
@@ -131,6 +136,8 @@ ActiveRecord::Schema.define(version: 2019_12_13_145419) do
     t.string "tel"
     t.string "raison_sociale"
     t.string "siret"
+    t.index ["email"], name: "index_prestataires_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_prestataires_on_reset_password_token", unique: true
   end
 
   create_table "services", force: :cascade do |t|
