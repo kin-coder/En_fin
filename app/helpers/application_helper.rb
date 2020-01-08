dadb8c34aa9bbc784af8fb100e54368c83bb10e3
menja-coder <rakotomenja@gmail.com>
2020-01-08 03:14:17 +0300

    ok

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    def resource_name
+        :client
+      end
+      
+      def resource
+        @resource ||= Client.new
+      end
+      
+      def resource_class
+        Client
+      end
+      
+      def devise_mapping
+        @devise_mapping ||= Devise.mappings[:client]
+      end
 end
