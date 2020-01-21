662a80fa7e26688ce738dbace3c9b180838a779b
RAMAHAFALY EDMOND <ramahafaly2.0@gmail.com>
2020-01-21 18:51:25 +0300

    ok

--- app/controllers/submitorders_controller.rb
+++ app/controllers/submitorders_controller.rb
@@ -1,12 +1,12 @@
 class SubmitordersController < ApplicationController
   def index_save
-    # Enregistreer la category_id et les souscategory_id dans un tableau [4,[5,4]]
+    # ----------------Enregistreer la category_id et les souscategory_id dans un tableau [4,[5,4]]
     subcategories = params[:prestations]
     orderPrestation = []
     subcategories.each do |k,subcategory|
       orderPrestation.push([Subcategory.find(subcategory[0]).category.id,subcategory])
     end
-    #Enregistrer les id des produits dans un tableau
+    # ----------------Enregistrer les id des produits dans un tableau
     produits = params[:products]
     orderProduit = [] #[nombre,leproduit]
     produits.uniq.each do |produit|
@@ -18,8 +18,23 @@ class SubmitordersController < ApplicationController
       end
       orderProduit.push([n,produit.to_i])
     end
-    # orderInformation [id(depart),adress,date,heure,message]
+    # ----------------orderInformation [id(depart),adress,date,heure,message]
     orderInformation = [params[:department],params[:Adresse],params[:Date],params[:Heure],params[:message]]
+=begin
+    #---------------------------------------------------------#
+    
+    orderPrestation.each do |prestation| # [ [id_category,[id_sub,id_sub]] ]
+
+    end
+    orderProduit.each do |produit| # [ [nombre,id_produit] ]
+
+    end
+    orderInformation.each do |information| # [ [id(depart),adress,date,heure,message] ]
+
+    end
+
+    #---------------------------------------------------------#
+=end
     puts "_"*50
     puts "liste des prestations"
     puts orderPrestation.inspect
@@ -32,6 +47,7 @@ class SubmitordersController < ApplicationController
     puts "_"*50
   	# ClientMailer.with(client: Client.last, commande: @commande1).validation_commande.deliver_now
   end
+
   def spa_reservation
   	puts "_"*150
   	puts params.inspect
