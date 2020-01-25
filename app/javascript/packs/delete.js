edf3ddfc55d929d7ba82db020987828758b05443
RAMAHAFALY EDMOND <ramahafaly2.0@gmail.com>
2020-01-25 06:27:56 +0300

    add page thanks

---
+++ app/javascript/packs/delete.js
@@ -0,0 +1,7 @@
+sessionStorage.clear();
+/*
+sessionStorage.setItem("service",form.dataset.service)
+sessionStorage.setItem("products","[]")
+sessionStorage.setItem("prestations","[]")
+sessionStorage.setItem("input",0)
+*/
