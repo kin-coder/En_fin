7848ad881a3ad8ee5383ad219fcfc74337112de4
RFaly <ramahafaly2.0@gmail.com>
2020-02-10 23:16:21 +0300

    add data to frontù

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,27 +1,99 @@
 class OrdersController < ApplicationController
+  # 1/2 Selection des prestation
   def index
     @countries = Country.all
     @departments = Department.all
-
-    # ========================================== #
-
     @services = Service.all
-
     # service location spa
-    @spas = Spa.all
-    @spaoptions = Product.where(is_option_spa:true)
-
+    @spas = []
+    tmpspa = Spa.all
+    tmpspa.each do |spa|
+      @spas.push([spa.duration,spa.exceptional_price,spa.exceptional_acompte,spa.ordinary_price,spa.ordinary_acompte])
+    end
+    # options pour location spa
+    @spaoptions = []
+    tmpoption = Product.where(is_option_spa:true)
+    tmpoption.each do |option|
+      @spaoptions.push([option.id,option.name,option.price])
+    end
     # service massage
-    @massages = MassageCa.all #.massage_sus liste sub sub[0].massage_su_prices //differen heurse
+    @massages = []
+    #.massage_sus liste sub sub[0].massage_su_prices //differen heurse
+    caMassages = MassageCa.all
+    # sou massage_ca Name  || massage_su Name ||
+    caMassages.each do |ca|
+      isCa = [ca.name,[]]
+      ca.massage_sus.each do |su|
+        isSu = [su.name,[]]
+        su.massage_su_prices.each do |price|
+          isSu[1].push([price.duration,price.exceptional_price,price.exceptional_acompte,price.ordinary_price,price.ordinary_acompte])
+        end
+        isCa[1].push(isSu)
+      end
+      @massages.push(isCa)
+    end
     @cadeaus = Product.where(is_option_spa:false)
-
     # ========================================== #
-
   end
+
+  # 2/2 Sauvegarder dans une session les données
+  def saveSession
+    puts "~~~~"*50
+    puts params.inspect
+    puts "~~~~"*50
+  end
+
+  # 2 Selection des adresse de livraison et facturation
+  def delivery
+    
+  end
+
+  # 3 Affiche la recapitulatif de commande
+  def summary
+    
+  end
+
+  # 4 Le Payement
+  def payment
+    
+  end
+
 
 end
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 =begin
+
+JSON.parse("Text to Data") // mamadika ny text ho data 
+
+JSON.stringify(Data) // mamadika ny data ho text
 
 
 @services = Service.all
