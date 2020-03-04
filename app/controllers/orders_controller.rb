80933f490749425c0f1287cb16148b1e92cc9807
RFaly <ramahafaly2.0@gmail.com>
2020-03-04 23:23:01 +0300

    summary and delivery back

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
   # 1/2 Selection des prestation
   def zone
-    @all_params = params.permit(:country,:department,:date,:heure)
+# "country"=>"France", "department"=>"17", "date"=>"03/24/2020"
     respond_to do |format|
        format.js
-    end 
+    end
   end
   
   def code_promo
@@ -62,88 +62,103 @@ class OrdersController < ApplicationController
   def saveSession
     # orderSpa = [{time:48,type:["a","b"],price:180,option:[["a",12],["c",45]]},{time:48,type:["a","b"],price:180,option:[["a",12],["c",45]]}]
     # orderMassage =  [{ca:"Homme",su:"prénatal",price:[30,120]}]
+
+    timeSpas = params[:timeSpa]
     orderSpa = []
-    params[:timeSpa].each do |k,v|
-      tmp = {}
-      if Spa.find_by(duration:v[0].to_i)
-        tmp["time"] = v[0].to_i
-      else
-        # erreur
-      end
-      tmp["type"] = params[:typeSpa][k]
-      options = params[:optionSpa][k]
-
-      if options
-        tmpOption = []
-        options.each do |option|
-          product = Product.find_by(name:option)
-          if product
-            tmpOption.push(option)
-          else
-            # erreur
-          end
-        end
-        tmp["option"] = tmpOption
-      end
-      orderSpa.push(tmp)
-    end
-
-    puts "~~~"*10
-    puts orderSpa
-    puts "~~~"*10
-
-    orderMassage = []
-
-    params[:massageSu].each do |k,v|
-      tmp = {}
-      mCa = MassageCa.find_by(name:v[0])
-      if mCa
-        tmp["ca"] = v[0]
-        mSu = mCa.massage_sus.find_by(name:v[1])
-        if mSu
-          tmp["su"] = v[1]
+    if timeSpas
+      timeSpas.each do |k,v|
+        tmp = {}
+        if Spa.find_by(duration:v[0].to_i)
+          tmp["time"] = v[0].to_i
         else
           # erreur
         end
-      else
-        # erreur
+        tmp["type"] = params[:typeSpa][k]
+        options = params[:optionSpa][k]
+        if options
+          tmpOption = []
+          options.each do |option|
+            product = Product.find_by(name:option)
+            if product
+              tmpOption.push(option)
+            else
+              # erreur
+            end
+          end
+          tmp["option"] = tmpOption
+        end
+        orderSpa.push(tmp)
       end
-      price = MassageSuPrice.find_by(duration:params[:massageSuPrice][k][0].to_i)
-      if price
-        tmp["price"] = [price.duration,price.ordinary_price,price.ordinary_acompte]
-      else
-        # erreur
-      end
-      orderMassage.push(tmp)
     end
 
-    puts "~~~"*10
-    puts orderMassage
-    puts "~~~"*10
+    orderMassage = []
+    massageSus = params[:massageSu]
+    if massageSus
+      massageSus.each do |k,v|
+        tmp = {}
+        mCa = MassageCa.find_by(name:v[0])
+        if mCa
+          tmp["ca"] = v[0]
+          mSu = mCa.massage_sus.find_by(name:v[1])
+          if mSu
+            tmp["su"] = v[1]
+          else
+            # erreur
+          end
+        else
+          # erreur
+        end
+        price = MassageSuPrice.find_by(duration:params[:massageSuPrice][k][0].to_i)
+        if price
+          tmp["price"] = [price.duration,price.ordinary_price,price.ordinary_acompte]
+        else
+          # erreur
+        end
+        orderMassage.push(tmp)
+      end
+    end
 
+    # params[:cadeau]
+    # <h3><%= session[:myPrestation] %></h3>
+    # <h3><%= session[:otherInfo] %></h3>
 
-    puts "~~praticien~~"*5
-    puts params[:praticien]
-    puts "~~~~"*5
+    session[:myPrestation] = {spa:orderSpa, massage:orderMassage}
+    session[:otherInfo] = {heureSpa:params[:heureSpa],praticien:params[:praticien],heureMassage:params[:heureMassage],cadeau:[]}
 
     redirect_to delivery_path
   end
 
   # 2 Selection des adresse de livraison et facturation
   def delivery
-    
 # email: ""
 # first_name: nil, last_name: nil, adresse: nil, tel: nil, sexe: nil>
   @client = Client.first
   @countries = Country.all
+  end
 
+  def saveDelivery
+    emptyIsInclude = params[:adresseL]=="" || params[:complAdresseL]=="" || params[:codePostaL]=="" || params[:villeL]=="" || params[:adresseF]=="" || params[:complAdresseF]=="" || params[:codePostaF]=="" || params[:villeF] =="" || params[:message]=="" || params[:countryL]=="" || params[:countryF]=="" || params[:countryL]==nil || params[:countryF]==nil
+    if emptyIsInclude
+      redirect_back(fallback_location: root_path)
+    else
+      session[:otherInfo]["adresseL"] = params[:adresseL]
+      session[:otherInfo]["complAdresseL"] = params[:complAdresseL]
+      session[:otherInfo]["codePostaL"] = params[:codePostaL]
+      session[:otherInfo]["villeL"] = params[:villeL]
+      session[:otherInfo]["countryL"] = params[:countryL]
+      session[:otherInfo]["adresseF"] = params[:adresseF]
+      session[:otherInfo]["complAdresseF"] = params[:complAdresseF]
+      session[:otherInfo]["codePostaF"] = params[:codePostaF]
+      session[:otherInfo]["villeF"] = params[:villeF]
+      session[:otherInfo]["countryF"] = params[:countryF]
+      session[:otherInfo]["message"] = params[:message]
+      redirect_to summary_path
+    end
   end
 
   # 3 Affiche la recapitulatif de commande
   def summary
-    puts "~~~~"*12
-    puts params.inspect
-    puts "~~~~"*12
+    
   end
 
   # 4 Le Payement
