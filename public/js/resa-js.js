d8db45f65e5eb31b958d2cd4222967ea50499ea6
RFaly <ramahafaly2.0@gmail.com>
2020-03-06 13:55:22 +0300

    bug in pop up fixed

--- public/js/resa-js.js
+++ public/js/resa-js.js
@@ -1,22 +1,33 @@
 initialize()
 
-
-
 function on(){
   document.getElementById("overlay").style.display = "block";
 }
 
 function initialize(){
-	let prestations = JSON.parse(sessionStorage.getItem("prestations"))
-	if (prestations.country != undefined) {
-		if (prestations.country != "") {
+	let zone = JSON.parse(sessionStorage.getItem("zone"))
+	if (zone != null && zone.country != undefined) {
+		if (zone.country != "") {
 			let valueCountry = document.getElementById('zode-and-date-selected')
 			let nameDepart = ""
-			if (prestations.department) {
-				nameDepart = "Departement : "+prestations.department
+			if (zone.department) {
+				nameDepart = "Departement : "+zone.department
 			}
-			valueCountry.innerHTML = "<div>Pays : "+prestations.country+" "+nameDepart+" Date de livraison : "+prestations.date+" <button class=\"btn btn-primary\" id=\"openZoneChoice\">Midfier votre zone et la date</button></div>"
+			valueCountry.innerHTML = "<div>Pays : "+zone.country+" "+nameDepart+" Date de livraison : "+zone.date+" <button class=\"btn btn-primary\" id=\"openZoneChoice\">Modifier</button></div>"
 			document.getElementById("openZoneChoice").addEventListener('click',openZone);
+		
+			if (zone.services.includes("Massage")){
+				document.getElementById("massage-fafana").classList.remove("hidden")
+			}else{
+				document.getElementById("massage-fafana").classList.add("hidden")
+			}
+
+			if (zone.services.includes("Location spa")){
+				document.getElementById("loc-spa-fafana").classList.remove("hidden")
+			}else{
+				document.getElementById("loc-spa-fafana").classList.add("hidden")
+			}
+
 		}else{
 			window.onload = on();
 		}
