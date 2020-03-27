63ca5d5bb04f92a6a6abbccec3ad75acc5d0daea
RFaly <ramahafaly2.0@gmail.com>
2020-03-27 21:20:16 +0300

    correct bug edit devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_devise_parameters, if: :devise_controller?
 
-    def configure_devise_parameters
-        devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name, :last_name, :adresse,:sexe, :tel, :email, :password, :password_confirmation, :country, :zip_code)}
-        devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name, :last_name, :sexe, :adresse, :tel, :email, :password, :password_confirmation, :country, :zip_code)}
-    end
+	def configure_devise_parameters
+	  devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name,:last_name,:adresse,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
+	  devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name,:last_name,:adresse,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
+	end
+
 end
