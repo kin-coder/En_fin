ef5d6c4d8efeebf230f101fac921050a407b173e
RFaly <ramahafaly2.0@gmail.com>
2020-04-07 16:43:00 +0300

    add client to admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -123,29 +123,20 @@ class AdminsController < Application2Controller
   # POST /clients.json
   def create
     @client = Client.new(client_params)
-
-    respond_to do |format|
-      if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
-        format.json { render :show, status: :created, location: @client }
-      else
-        format.html { render :new }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
+    if @client.save
+      redirect_to index_clients_path, notice: 'Client was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /clients/1
   # PATCH/PUT /clients/1.json
   def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @client }
-      else
-        format.html { render :edit }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
+    if @client.update(client_params)
+      redirect_to show_clients_path(@client), notice: 'Client was successfully updated.'
+    else
+      render :edit
     end
   end
 
@@ -153,10 +144,7 @@ class AdminsController < Application2Controller
   # DELETE /clients/1.json
   def destroy
     @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to index_clients_path, notice: 'Client was successfully destroyed.'
   end
 
   private
@@ -167,7 +155,7 @@ class AdminsController < Application2Controller
 
     # Only allow a list of trusted parameters through.
     def client_params
-      params.require(:client).permit(:first_name, :last_name, :adresse, :tel, :sexe, :country, :zip_code, :email)
+      params.require(:client).permit(:first_name, :last_name, :adresse, :tel, :sexe, :country, :zip_code, :email, :password, :password_confirmation)
     end
 
 end
