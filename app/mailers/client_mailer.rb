6917afacacf13b6fefeb2d90402478144ddea0e7
RFaly <ramahafaly2.0@gmail.com>
2020-04-16 12:10:33 +0300

    mailer object edit for client

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,11 +1,10 @@
 class ClientMailer < ApplicationMailer
+
   def confirm_order(order_id,client_id)
     @client = Client.find(client_id)
     @order = Order.find(order_id)
-    
     @date = @order.prestation_date.split("/")
     @date = "#{@date[1]}/#{@date[0]}/#{@date[2]}"
-
     @heurs = ["",""]
     @order.order_services.each do |o_s|
     	if o_s.service.name == "Location spa"
@@ -14,9 +13,8 @@ class ClientMailer < ApplicationMailer
     		@heurs[1] = o_s.service_time
     	end
     end
-
     mail(to: @client.email, subject: 'Votre commande Cocooning Spa !')
-	end
+  end
 
 	def abandoned_order
 		
