b7279edf1129e060c82beed8c6660e4f4074534a
menja-coder <rakotomenja@gmail.com>
2020-05-14 14:47:01 +0300

    page profil ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,13 @@ class ApplicationController < ActionController::Base
 	before_action :configure_devise_parameters, if: :devise_controller?
 
 	def configure_devise_parameters
-	  devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name,:last_name,:adresse,:adresse_complet,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
-	  devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name,:last_name,:adresse,:adresse_complet,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
+		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name,:last_name,:adresse,:adresse_complet,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
+		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name,:last_name,:adresse,:adresse_complet,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
 	end
-def after_sign_in_path_for(resource_or_scope)
-  session.fetch 'user_return_to', client_profil_path
-end
+
+	# redirection after sign in
+	
+	def after_sign_in_path_for(resource_or_scope)
+		session.fetch 'user_return_to', client_profil_path
+	end
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -15,6 +15,11 @@ class ClientsController < ApplicationController
 
   def order #show d'une order
     @order = Order.find(params[:id])
+
+    # pour avoir l'heure de chaque prestation
+    @service_spa = @order.order_services.where(service_id:'2')
+    @service_massage = @order.order_services.where(service_id:'1')
+
     if @order.client.id != current_client.id
       redirect_to client_profil_path
     end
