5f317f64e7af7a626ba7662adad8a8e3d3ac5416
RFaly <ramahafaly2.0@gmail.com>
2020-05-15 18:03:58 +0300

    row all

--- app/javascript/packs/orders_index.js
+++ app/javascript/packs/orders_index.js
@@ -4,10 +4,33 @@ $(".remove-massage").click(function() {
 });
 
 $(".add-massage").click(function() {
-	$gona = $("#"+$(this).data().category).html()
-	$("#select-list-massage").append($gona);
-	console.log($gona)
-	numberOfPrestationMassage($(this).data().category);
+	$prestationGroups = $($("#"+$(this).data().category).html())
+	let category = $(this).data().category;
+	let longeur = $(".prestation-group[data-category='"+category+"']").length;
+/*---------------------GERER LES ID ET LES ATTRIBUTS DES INPUT --------------------*/
+	if (category === "for_massage_man") {
+		$prestationGroups.find("input").each(function(index, element){
+			let valueAttribute = $(element).attr("id")
+			$(element).attr("id",valueAttribute+"man"+longeur)
+		});
+		$prestationGroups.find("label").each(function(index, element){
+			let valueAttribute = $(element).attr("for")
+			$(element).attr("for",valueAttribute+"man"+longeur)
+		});
+	}
+	if (category === "for_massage_woman") {
+		$prestationGroups.find("input").each(function(index, element){
+			let valueAttribute = $(element).attr("id")
+			$(element).attr("id",valueAttribute+"woman"+longeur)
+		});
+		$prestationGroups.find("label").each(function(index, element){
+			let valueAttribute = $(element).attr("for")
+			$(element).attr("for",valueAttribute+"woman"+longeur)
+		});
+	}
+/*---------------------GERER LES ID ET LES ATTRIBUTS DES INPUT --------------------*/
+	$("#select-list-massage").append($prestationGroups);
+	numberOfPrestationMassage(category);
 });
 
 function numberOfPrestationMassage(category) {
@@ -18,4 +41,39 @@ function numberOfPrestationMassage(category) {
 	if (category === "for_massage_woman") {
 		$("#number_woman").text(list_length);
 	}
+	return list_length;
 }
+
+
+/*
+
+
+
+function initSession(){
+	sessionStorage.setItem("prestations","{}")
+	sessionStorage.setItem("spa","[]")
+	sessionStorage.setItem("massages","[]")
+	sessionStorage.setItem("cadeau","[]")
+	sessionStorage.setItem("inc","0")
+}
+
+
+sessionStorage.setItem("prestations",JSON.stringify(prestations))
+let prestations = JSON.parse(sessionStorage.getItem("prestations"))
+
+[
+{
+"category":"man",
+"prestations":["man_shampoo","man_haircut"]
+},
+
+{
+"category":"woman",
+"prestations":["woman_shampoo","woman_color"]
+}
+
+]
+
+
+*/
+
