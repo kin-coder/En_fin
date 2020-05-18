1d9b1df60ae7bd7580b227d8ba9b4953b585d980
RFaly <ramahafaly2.0@gmail.com>
2020-05-18 16:34:01 +0300

    new cgu

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,8 +3,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
   # acivation letter_opener
-  # config.action_mailer.delivery_method = :letter_opener
-  
+  config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
   # In the development environment your application's code is reloaded on
