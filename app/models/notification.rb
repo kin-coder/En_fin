fc0901b778eb5a869b6b252a4d098b88b6687c52
RFaly <ramahafaly2.0@gmail.com>
2020-05-20 12:36:14 +0300

    save

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,10 @@
 class Notification < ApplicationRecord
+	def url
+		if self.notif_type == 1
+				return "/admin/commande-numero/#{(JSON.parse(self.data)["id"])}"
+		end
+	end
+	def messages
+		return JSON.parse(self.data)["text"]
+	end
 end
