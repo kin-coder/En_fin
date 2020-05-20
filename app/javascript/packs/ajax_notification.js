6cb7c00b49cfc8375029bf3643922343b20bcd03
RFaly <ramahafaly2.0@gmail.com>
2020-05-20 16:45:49 +0300

    save data for ajax

--- app/javascript/packs/ajax_notification.js
+++ app/javascript/packs/ajax_notification.js
@@ -6,16 +6,12 @@ function requeteAjax(){
 	xhr.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
 			data = this.response
-			if (data.length > 0) {
-				console.log(data)
+			if (data.length > 0){
 				for (var i = data.length - 1; i >= 0; i--) {
-					var nodeLi = '<li><a data-view="'+data[i].notif.view+'" rel="nofollow" data-method="patch" href="/admin/notification/'+data[i].notif.id+'">'+ data[i].notif.message +'</a><li>'
-					ulContainer.innerHTML = nodeLi + ulContainer.innerHTML
+					ulContainer.innerHTML = '<li><a data-view="'+data[i].notif.view+'" rel="nofollow" data-method="patch" href="/admin/notification/'+data[i].notif.id+'">'+ data[i].notif.message +'</a><li>' + ulContainer.innerHTML
 				}
 				newNotification.innerHTML = parseInt(newNotification.innerHTML)+data.length
 				document.getElementById("notification_list").dataset.notification = data[data.length-1].notif.id+1
-			}else{
-				console.log("tsy misy")
 			}
 		}
 	};
