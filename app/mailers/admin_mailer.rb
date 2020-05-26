681e802afc93bafc2ea777e60df8e54e06e954df
RFaly <ramahafaly2.0@gmail.com>
2020-05-26 16:48:29 +0300

    ok

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,14 +1,14 @@
 class AdminMailer < ApplicationMailer
 	def contac_us(message)
 		@message = message
-		@url = 'http://spamandonabe.herokuapp.com/'
+		@url = 'http://spamandona.herokuapp.com/'
 		# permet d'envoyer l'email à to: ...
 		mail(to: 'cocooningspa@outlook.com', subject: "Un client a besoin d'information ou d'aide")
 	end
 
 	def new_partner(candidate)
 		@candidate = candidate
-		@url = 'http://spamandonabe.herokuapp.com/'
+		@url = 'http://spamandona.herokuapp.com/'
 		# permet d'envoyer l'email à to: ...
 		mail(to: 'cocooningspa@outlook.com', subject: '​Nouvelle demande de partenariat !')
 	end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # configuration en production localhost a changé
   
-  config.action_mailer.default_url_options = { :host => 'spamandonabe.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'spamandona.herokuapp.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
