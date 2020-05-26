13e505e252176411b31f9dcfb7b8816e629bddd5
menja-coder <rakotomenja@gmail.com>
2020-05-26 15:32:05 +0300

    offf

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -3,14 +3,14 @@ class AdminMailer < ApplicationMailer
 		@message = message
 		@url = 'http://spamandona.herokuapp.com/'
 		# permet d'envoyer l'email à to: ...
-		mail(to: 'michael77rakotovao@gmail.com', subject: "Un client a besoin d'information ou d'aide")
+		mail(to: 'cocooningspa@outlook.com', subject: "Un client a besoin d'information ou d'aide")
 	end
 
 	def new_partner(candidate)
 		@candidate = candidate
 		@url = 'http://spamandona.herokuapp.com/'
 		# permet d'envoyer l'email à to: ...
-		mail(to: 'michael77rakotovao@gmail.com', subject: '​Nouvelle demande de partenariat !')
+		mail(to: 'cocooningspa@outlook.com', subject: '​Nouvelle demande de partenariat !')
 	end
 
 end
