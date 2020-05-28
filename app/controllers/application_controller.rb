b3d6f064bb2c518362fa3ae837de0a48a89cd3cb
RFaly <ramahafaly2.0@gmail.com>
2020-05-28 19:02:07 +0300

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,8 @@ class ApplicationController < ActionController::Base
 		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:first_name,:last_name,:adresse,:adresse_complet,:town,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
 		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:first_name,:last_name,:adresse,:adresse_complet,:town,:tel,:sexe,:country,:zip_code, :email, :password, :current_password, :password_confirmation)}
 	end
-
 	# redirection after sign in
-	
 	def after_sign_in_path_for(resource_or_scope)
 		session.fetch 'user_return_to', client_profil_path
 	end
-
-	def after_inactive_sign_up_path_for(resource)
-    cgu_path # <- Path you want to redirect the user to.
-  end
-
 end
