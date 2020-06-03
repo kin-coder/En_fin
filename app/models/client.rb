d52cbbf83831fcb244e8d63ddeeb542f99cc21c5
itokiana <itokianarajoelison@gmail.com>
2020-06-03 15:53:28 +0300

    Modif 6 Itokiana

--- app/models/client.rb
+++ app/models/client.rb
@@ -12,5 +12,5 @@ class Client < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable#, :confirmable
 end
